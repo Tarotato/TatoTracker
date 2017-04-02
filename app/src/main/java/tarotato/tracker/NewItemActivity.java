0c3c77a07cadc981553af3eef9159e22ea85c08d
Isabel Zhuang <isabelz123@gmail.com>
2017-04-02 13:37:08 +1200

    Added save button to action bar in NewItem class

--- app/src/main/java/tarotato/tracker/NewItemActivity.java
+++ app/src/main/java/tarotato/tracker/NewItemActivity.java
@@ -2,7 +2,10 @@ package tarotato.tracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class NewItemActivity extends AppCompatActivity {
 
@@ -16,11 +19,22 @@ public class NewItemActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //Create "Save" button
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_new_item, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case android.R.id.home:
                 finish();
                 return true;
+            case R.id.save_action:
+                Toast.makeText(this, "Save selected", Toast.LENGTH_SHORT)
+                        .show();
             default:
                 return super.onOptionsItemSelected(item);
         }
