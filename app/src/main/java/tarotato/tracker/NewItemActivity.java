59f1ba1dd8f3c7f7b876f23c2584c636f40a3dbf
Isabel Zhuang <isabelz123@gmail.com>
2017-04-26 15:48:29 +1200

    Added elements to new item layout

--- app/src/main/java/tarotato/tracker/NewItemActivity.java
+++ app/src/main/java/tarotato/tracker/NewItemActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class NewItemActivity extends AppCompatActivity {
@@ -16,6 +18,15 @@ public class NewItemActivity extends AppCompatActivity {
         //Enable back arrow button
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.item_tags, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     @Override
